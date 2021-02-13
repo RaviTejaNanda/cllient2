660a6cbe9134abaa22cc27925d349526ec9f36b0
suneetha <nanda.ravi28@gmail.com>
2021-02-12 22:43:41 -0500

    hi added

--- src/main/java/com/n2s/cllient2/controller/Client2controller.java
+++ src/main/java/com/n2s/cllient2/controller/Client2controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//hi hr u
 @RestController
 public class Client2controller {
 @Autowired
